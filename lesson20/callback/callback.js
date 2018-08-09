babead36b798fcbad99c065b22536aac4c25f6b2
kob-e <kobieshka@gmail.com>
2018-08-09 20:14:04 +0300

    callback fix

--- lesson20/callback/callback.js
+++ lesson20/callback/callback.js
@@ -1,10 +1,14 @@
 
 
 
-function h() {
-    for (let i=0; i < 5; i++) {
-        setTimeout(function () {
-            console.log(i);
-        }, i * 1000)
-    }
+for (var i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, i * 1000)
+}
+
+for (let j=0; j < 5; j++) {
+    setTimeout(function () {
+        console.log(j);
+    }, i * 1000)
 }
