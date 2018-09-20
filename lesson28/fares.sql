048b0cfa7b5efa560d5de20ff7a631e875f04337
yaakov hatam <kobieshka@gmail.com>
2018-09-20 21:56:46 +0300

    Update fares.sql

--- lesson28/fares.sql
+++ lesson28/fares.sql
@@ -20,3 +20,26 @@ INSERT INTO `flights` (`flight_id`, `flight_from`, `flight_to`, `flight_datetime
 (8, 'Eilat', 'Milano', '2018-11-14 20:15:36', NULL),
 (9, 'Eilat', 'Marseille', '2018-10-27 20:15:36', NULL),
 (10, 'Eilat', 'Amsterdam', '2018-10-11 20:15:36', NULL);
+
+
+UPDATE flights SET `flight_special_comments` = "Important: Check in 3 hours before" 
+WHERE `flight_from` LIKE "Eilat"
+
+/* auto icrement select id inserted */
+INSERT INTO `flights` (`flight_from`, `flight_to`, `flight_datetime`, `flight_special_comments`) VALUES
+('Tel aviv', 'London', '2018-09-20 19:23:24', NULL),
+ SELECT LAST_INSERT_ID();
+
+
+/* insert with auto increment */
+    INSERT INTO `flights` (`flight_from`, `flight_to`, `flight_datetime`, `flight_special_comments`) VALUES
+    ('Tel aviv', 'London', '2018-09-20 19:23:24', ""),
+    ('Tel aviv', 'Paris', '2018-09-27 19:24:22', ""),
+    ('Tel aviv', 'Budapest', '2018-09-20 19:26:14', ""),
+    ('Tel aviv', 'Amsterdam', '2018-09-22 20:15:36', ""),
+    ('Tel aviv', 'Marseille', '2018-09-30 20:15:36', ""),
+    ('Tel aviv', 'Barcelona', '2018-09-20 20:15:36', ""),
+    ('Tel aviv', 'Lazio', '2018-10-17 20:15:36', ""),
+    ('Eilat', 'Milano', '2018-11-14 20:15:36', ""),
+    ('Eilat', 'Marseille', '2018-10-27 20:15:36', ""),
+    ( 'Eilat', 'Amsterdam', '2018-10-11 20:15:36', "");
