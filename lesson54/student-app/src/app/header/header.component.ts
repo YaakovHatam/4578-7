9d56ddf61f3e378ebcf73f0f4165acefa2c441f0
kob-e <kobieshka@gmail.com>
2018-12-31 21:22:09 +0200

    angular lesson54 tepmlate

--- lesson54/student-app/src/app/header/header.component.ts
+++ lesson54/student-app/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../models/student.model';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  title: string;
+  menuItems: string[];
+  show: boolean;
+  hclass: string;
+  colorClass: string[];
+  students: Student[];
 
   constructor() { }
 
   ngOnInit() {
+    this.colorClass = [
+      'blue',
+      'red',
+      'brown'
+    ];
+    this.hclass = this.colorClass[0];
+    this.show = true;
+    this.title = 'i am title';
+    this.menuItems = [
+      'Home',
+      'About',
+      'Contact'
+    ];
+
+    this.students = [
+      {
+        id: 1,
+        name: 'student 1'
+      },
+      {
+        id: 2,
+        name: 'student 2'
+      },
+      {
+        id: 3,
+        name: 'student 2'
+      }
+    ]
+  }
+
+  clicked() {
+    this.hclass = this.colorClass[Math.floor(Math.random()*3)];
   }
 
 }
