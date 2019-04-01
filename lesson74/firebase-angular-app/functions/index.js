74c236a72bbe35323a82b5efe4205ad75ae69e28
kob-e <kobieshka@gmail.com>
2019-04-01 21:19:06 +0300

    firebase function

--- lesson74/firebase-angular-app/functions/index.js
+++ lesson74/firebase-angular-app/functions/index.js
@@ -1,8 +1,9 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.createStudent = functions.firestore.document('students/{studentId}').onCreate((snap, context) => {
+    return snap.ref.set({
+        created_timestamp: new Date()
+    }, {
+        merge: true
+    });
+});
